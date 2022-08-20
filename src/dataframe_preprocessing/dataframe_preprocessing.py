c0a8452726ad687d94350086704ffbb1b0ff5dfe
Ioannis-Mykelos <54843968+Ioannis-Mykelos@users.noreply.github.com>
2022-08-20 20:33:59 +0200

    encoding

--- src/dataframe_preprocessing/dataframe_preprocessing.py
+++ src/dataframe_preprocessing/dataframe_preprocessing.py
@@ -3,3 +3,27 @@ dataframe preprocessing file
 """
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def encoding_the_columns(dataframe: pd.DataFrame)->pd.DataFrame:
+    """
+    This function encodes the columns
+
+    Arguments:
+    -----------------------------------------
+    -dataframe  :   The initial dataframe
+    
+    Returns:
+    -----------------------------------------
+    -dataframe  :   dataframe
+    """
+
+    # Instantiate LabelEncoder
+    le=LabelEncoder()
+    for column in dataframe.columns.to_numpy():
+        # Compare if the dtype is object
+        if dataframe[column].dtypes=='object':
+            # Use LabelEncoder to do the numeric transformation
+            dataframe[column]=le.fit_transform(dataframe[column])
+    return dataframe
+    
