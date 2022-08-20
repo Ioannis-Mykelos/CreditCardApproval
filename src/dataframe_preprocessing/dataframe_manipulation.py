6c053106c8af41864a830c85318f375666e65dd9
Ioannis-Mykelos <54843968+Ioannis-Mykelos@users.noreply.github.com>
2022-08-20 19:41:30 +0200

    start the project

--- src/dataframe_preprocessing/dataframe_manipulation.py
+++ src/dataframe_preprocessing/dataframe_manipulation.py
@@ -1,3 +1,20 @@
 """
 dataframe manipulation file
 """
+
+import pandas as pd
+
+def load_data()->pd.DataFrame:
+    """
+    This function loads the initial dataframe
+
+    Arguments:
+    -----------------------------------------
+    -None       :    Nothing
+
+    Returns:
+    -----------------------------------------
+    -dataframe  :    The credit cards dataframe
+    """
+    dataframe = pd.read_csv("data/cc_approvals.data", header=None)
+    return dataframe
