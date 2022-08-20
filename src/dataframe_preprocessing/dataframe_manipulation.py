0ac9ee49611908ba37638e5ba9e8086c1344f887
Ioannis-Mykelos <54843968+Ioannis-Mykelos@users.noreply.github.com>
2022-08-20 20:05:02 +0200

    update

--- src/dataframe_preprocessing/dataframe_manipulation.py
+++ src/dataframe_preprocessing/dataframe_manipulation.py
@@ -1,7 +1,7 @@
 """
 dataframe manipulation file
 """
-
+import numpy as np
 import pandas as pd
 
 def load_data()->pd.DataFrame:
@@ -17,4 +17,24 @@ def load_data()->pd.DataFrame:
     -dataframe  :    The credit cards dataframe
     """
     dataframe = pd.read_csv("data/cc_approvals.data", header=None)
+    return dataframe
+
+def handle_nan_values(dataframe: pd.DataFrame)->pd.DataFrame:
+    """
+    This function handles the special characters and NaN values
+
+    Arguments:
+    -----------------------------------------
+    -dataframe  :   The initial dataframe
+    
+    Returns:
+    -----------------------------------------
+    -dataframe  :   dataframe
+    """
+    dataframe = dataframe.replace('?', np.nan)
+    dataframe.fillna(dataframe.mean(), inplace=True)
+    for col in dataframe.columns:
+        if dataframe[col].dtypes == 'object':
+            # Impute with the most frequent value
+            dataframe = dataframe.fillna(dataframe[col].value_counts().index[0])
     return dataframe
